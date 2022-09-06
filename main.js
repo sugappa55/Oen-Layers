99c6ee554db60d4cd3b7e161ffb2a1a383f32ee0
KurubaSugappa <kuruba.sugappa@attentve.ai>
2022-09-06 23:20:33 +0530

    added functionality of layer switching

--- main.js
+++ main.js
@@ -4,8 +4,8 @@ import Modify from 'ol/interaction/Modify';
 
 import Map from 'ol/Map';
 import View from 'ol/View';
-import {OSM, Vector as VectorSource} from 'ol/source';
-import {Tile as TileLayer, Vector as VectorLayer} from 'ol/layer';
+import {OSM, Raster, Vector as VectorSource, XYZ} from 'ol/source';
+import {Group, Tile as TileLayer, Vector as VectorLayer} from 'ol/layer';
 
 const raster = new TileLayer({
   source: new OSM(),
@@ -15,10 +15,12 @@ const source = new VectorSource({wrapX: false});
 
 const vector = new VectorLayer({
   source: source,
+  // visible:true,
+  // title:"Vector"
 });
 
 const map = new Map({
-  layers: [raster, vector],
+  // layers: [raster, vector],
   target: 'map',
   view: new View({
     center: [0, 0],
@@ -29,11 +31,49 @@ const map = new Map({
 //Base map Layers
 
 
+const Humanitarian=new TileLayer({
+  source:new OSM({
+    url:"https://{a-c}.tile.openstreetmap.fr/hot/{z}/{x}/{y}.png"
+  }),
+  visible:false,
+  title:"Humanitarian"
+})
 
 
+const Standard=new TileLayer({
+  source:new OSM(),
+  visible:true,
+  title:"Standard"
+})
 
 
+const Stamen=new TileLayer({
+source:new XYZ({
+  url:"https://stamen-tiles.a.ssl.fastly.net/terrain/{z}/{x}/{y}.jpg",
+  attributions:'Map tiles by <a href="http://stamen.com">Stamen Design</a>, under <a href="http://creativecommons.org/licenses/by/3.0">CC BY 3.0</a>. Data by <a href="http://openstreetmap.org">OpenStreetMap</a>, under <a href="http://www.openstreetmap.org/copyright">ODbL</a>.',
+ 
+}),
+visible:false,
+title:"Stamen"
+})
 
+const allLayers=new Group({
+  layers:[Standard,Humanitarian,Stamen,vector ]
+})
+
+map.addLayer(allLayers)
+
+
+const LayerElements=document.querySelectorAll("#layers>input[type=radio]")
+for (const layer of LayerElements) {
+      layer.addEventListener("change",function(){
+            var LayerValue=this.value;
+            allLayers.getLayers().forEach(elem=>{
+              let layerTitle=elem.get("title")
+              if(layerTitle) elem.setVisible(layerTitle===LayerValue)
+            })
+      })
+}
 
 
 
@@ -66,7 +106,6 @@ switcher.addEventListener("click",()=>{
   const layers=document.getElementById("layers")
   // layers.classList.add("switcher-visible")
   layers.style.right="1%"
-  console.log(layers)
 })
 
 const closeLayers=document.getElementById("close-layers")
